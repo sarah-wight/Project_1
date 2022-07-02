732abe7fb1eb70cc068f055d02241dbab1939e12
sarah-wight <107153217+sarah-wight@users.noreply.github.com>
2022-07-02 12:41:35 -0400

    Update Record.java

--- src/Project1/Record.java
+++ src/Project1/Record.java
@@ -10,14 +10,47 @@ import java.util.ArrayList;
  *
  */
 public class Record {
-    //private Date date; need a date class
+    private Date date; // need a date class
     private Student student;
     private Course course;
     private float grade;
 
-    /*public Record(Date date, Student student, Course course, float grade) {
-        what goes in here
-    }*/
+    public Record(Date date, Student student, Course course, float grade) {
+        this.date = date;
+        this.student = student;
+        this.course = course;
+        this.grade = grade;
+    }
 
+    public Date getDate() {
+        return date;
+    }
 
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public float getGrade() {
+        return grade;
+    }
+
+    public void setGrade(float grade) {
+        this.grade = grade;
+    }
 }
