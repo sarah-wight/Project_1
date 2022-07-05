b1161c3e6bb2159086739e1c5176a3bba3ae2297
sarah-wight <107153217+sarah-wight@users.noreply.github.com>
2022-07-05 12:48:24 -0400

    Create RecordsMenu.java

---
+++ src/Project1/RecordsMenu.java
@@ -0,0 +1,45 @@
+package Project1;
+import java.util.Scanner;
+
+/**
+ * Records Menu class.
+ *
+ * This file allows the creation of new records, viewing transcripts,
+ * saving to or loading records from a disk, or return to the main menu
+ * @author Sarah Wight, Zack Peters, Mike Rosin
+ * @date 7/6/2022
+ *
+ */
+public class RecordsMenu {
+    private Scanner scnr = new Scanner(System.in);
+
+
+    /**
+     * addRecord.  Prompts user for all required information
+     * for a record.
+     */
+    public void addRecord() {
+        // Adding a record selects an existing student, an existing course and
+        // adds a grade and date for how and when the student completed the course.
+    }
+
+    /**
+     * viewTranscript.  Displays the transcript of a student
+     */
+    public void viewTranscript() {
+
+    }
+
+    /**
+     * saveRecord.  Saves the record to a disk
+     */
+    public void saveRecord() {
+
+    }
+
+    /**
+     * loadRecord.  Loads the record from a disk
+     */
+    public void loadRecord() {
+    }
+}
