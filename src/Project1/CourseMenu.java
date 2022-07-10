9790c91ec21577e8b91f3058954fbb9777d992b1
sarah-wight <107153217+sarah-wight@users.noreply.github.com>
2022-07-10 13:27:20 -0400

    updated with try catch blocks

--- src/Project1/CourseMenu.java
+++ src/Project1/CourseMenu.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
  * This file can add new courses
  *  * @author Sarah Wight, Zack Peters, Mike Rosin
  * @author Sarah Wight, Zack Peters, Mike Rosin
- * @date 7/6/2022
+ * @date 7/11/2022
  *
  */
 public class CourseMenu {
@@ -17,22 +17,53 @@ public class CourseMenu {
     private Scanner scnr = new Scanner(System.in);
     private Record record = new Record();
     private SMS sms = new SMS();
+    private boolean valid = false;
 
-
-    // where do we store these courses/students when done?
+    /*********************************************************
+     * addCourse.  Prompts user for all required information
+     * about a course.
+     ********************************************************/
     public void addCourse() {
+        Course course = new Course(); // creating new course
 
-        Course course = new Course();
-        // Prompting user to enter student information
-        System.out.println("Enter course prefix");
-        course.setPrefix(scnr.nextLine());
+        // Prompting user to enter course prefix
+        while(!valid) {
+            try {
+                System.out.println("Enter course prefix");
+                course.setPrefix(scnr.nextLine());
+                valid = true; // if valid entry, break out of loop
+            } catch (RuntimeException ex) {
+                System.out.println(ex.getMessage());
+                System.out.println("Please try again\n");
+            }
+        }
 
-        System.out.println("Enter course number");
-        course.setNumber(scnr.nextInt());
-        scnr.nextLine();
+        // Prompting user to enter course number
+        valid = false;
+        while(!valid) {
+            try {
+                System.out.println("Enter course number");
+                course.setNumber(scnr.nextInt());
+                scnr.nextLine();
+                valid = true; // if valid entry, break out of loop
+            } catch (RuntimeException ex) {
+                System.out.println(ex.getMessage());
+                System.out.println("Please try again\n");
+            }
+        }
 
-        System.out.println("Enter course title");
-        course.setTitle(scnr.nextLine());
+        // Prompting user to enter course title
+        valid = false;
+        while(!valid) {
+            try {
+                System.out.println("Enter course title");
+                course.setTitle(scnr.nextLine());
+                valid = true; // if valid entry, break out of loop
+            } catch (RuntimeException ex) {
+                System.out.println(ex.getMessage());
+                System.out.println("Please try again\n");
+            }
+        }
 
         sms.addC(course); // adding new course to the list of courses
         sms.printC();
