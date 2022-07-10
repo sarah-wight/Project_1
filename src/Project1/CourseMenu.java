884b83cd6be2ae071c9b6607436b65b36535c583
sarah-wight <107153217+sarah-wight@users.noreply.github.com>
2022-07-10 12:23:50 -0400

    Update CourseMenu.java

--- src/Project1/CourseMenu.java
+++ src/Project1/CourseMenu.java
@@ -1,15 +1,28 @@
 package Project1;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * Course Menu class.
+ *
+ * This file can add new courses
+ *  * @author Sarah Wight, Zack Peters, Mike Rosin
+ * @author Sarah Wight, Zack Peters, Mike Rosin
+ * @date 7/6/2022
+ *
+ */
 public class CourseMenu {
     private Course course = new Course();
     private Scanner scnr = new Scanner(System.in);
     private Record record = new Record();
     private SMS sms = new SMS();
 
+
     // where do we store these courses/students when done?
     public void addCourse() {
 
+        Course course = new Course();
         // Prompting user to enter student information
         System.out.println("Enter course prefix");
         course.setPrefix(scnr.nextLine());
@@ -21,9 +34,8 @@ public class CourseMenu {
         System.out.println("Enter course title");
         course.setTitle(scnr.nextLine());
 
-        //record.setCourse(course);
-
-        System.out.println(course);
+        sms.addC(course); // adding new course to the list of courses
+        sms.printC();
 
     }
 }
