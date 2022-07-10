94fa38fd7c9f28f331a0c8b85b86a24b5981dbe5
sarah-wight <107153217+sarah-wight@users.noreply.github.com>
2022-07-10 12:24:48 -0400

    Update MainMenu.java

--- src/Project1/MainMenu.java
+++ src/Project1/MainMenu.java
@@ -1,4 +1,5 @@
 package Project1;
+
 import java.util.Scanner;
 
 /**
@@ -7,58 +8,26 @@ import java.util.Scanner;
  * This file displays the main menu, student, course, and record menus.
  * It takes user input as a menu selection and displays the appropriate menu
  * @author Sarah Wight, Zack Peters, Mike Rosin
- * @date 7/6/2022
+ * @date 7/11/2022
  *
  */
 public class MainMenu {
-    /**
-     * Main.  Allows program to run. Displays the main menu and
-     * takes user input that directs to either the student,
-     * course, or record menus.
-     */
-    public static void main(String[] args) {
-        // initializing variables
-        Scanner scnr = new Scanner(System.in);
-        int userNum = 0;
-        int menuChoice = 0;
 
-        // Displaying main menu - move this out of main later
-        System.out.println("Main Menu\n");
-        System.out.println("==========\n");
-        System.out.println("1. Work with Students\n");
-        System.out.println("2. Work with Courses\n");
-        System.out.println("3. Work with Records\n");
-        System.out.println("4. Quit\n");
+    private StudentMenu sm = new StudentMenu(); // creating object of the student menu class
+    private CourseMenu cm = new CourseMenu(); // creating object of the course menu class
+    private RecordsMenu rm = new RecordsMenu(); // creating object of the student menu class
+    private Scanner scnr = new Scanner(System.in);
 
-        userNum = scnr.nextInt(); // takes user input for menu choice
-
-        // different menu displayed based on user input
-        switch(userNum) {
-            case 1: studentMenu(); // if 1, student menu is displayed
-                    break;
-            case 2: courseMenu();
-                    break;
-            case 3: menuChoice = 3;
-                    break;
-            case 4: menuChoice = 4;
-                    break;
-            default: menuChoice = 0;
-        }
-
-        System.out.println(menuChoice);
-    }
-
-    /**
+    /***************************************************************
      * displayMain.  Displays the main menu and takes user input that
      * directs to either the student, course, or record menus.
-     */
-    public static void displayMain() {
-        // initializing variables
-        Scanner scnr = new Scanner(System.in);
-        int userNum = 0;
-        int menuChoice = 0;
+     **************************************************************/
+    public void displayMain() {
 
-        // Displaying main menu - move this out of main later
+        // initializing variables
+        int userNum = 0;
+
+        // Displaying main menu
         System.out.println("Main Menu\n");
         System.out.println("==========\n");
         System.out.println("1. Work with Students\n");
@@ -69,31 +38,34 @@ public class MainMenu {
         userNum = scnr.nextInt(); // takes user input for menu choice
 
         // different menu displayed based on user input
-        switch(userNum) {
-            case 1: studentMenu(); // if 1, student menu is displayed
+        switch (userNum) {
+            case 1:
+                studentMenu(); // if 1, student menu is displayed
                 break;
-            case 2: courseMenu(); // if 2, course menu
+            case 2:
+                courseMenu(); // if 2, course menu is displayed
                 break;
-            case 3: menuChoice = 3;
+            case 3:
+                recordsMenu(); // if , record menu is displayed
                 break;
-            case 4: menuChoice = 4;
+            case 4:
+                break; // if 4, break
+            default:
+                System.out.println("Must choose an option");
+                displayMain();
                 break;
-            default: menuChoice = 0;
         }
-
-        //System.out.println(menuChoice);
+        displayMain();
     }
 
-    /**
+    /***************************************************************
      * studentMenu.  Displays student menu and takes user input
      * to either add/modify a student or go back to the main menu.
-     */
-    public static void studentMenu() {
+     **************************************************************/
+    public void studentMenu() {
+
         // initializing variables
-        StudentMenu sm = new StudentMenu(); // creating object of the student menu class
-        Scanner scnr = new Scanner(System.in); // scanner
         int userNum = 0;
-        int menuChoice = 0;
 
         // Displaying student menu
         System.out.println("Student Menu\n");
@@ -105,31 +77,32 @@ public class MainMenu {
         userNum = scnr.nextInt(); // taking user input for menu selection
 
         // action is based on user input
-        switch(userNum) {
-            case 1: sm.addStudent(); // if 1, a student will be added
+        switch (userNum) {
+            case 1:
+                sm.addStudent(); // if 1, a student will be added
                 break;
-            case 2: System.out.println("Enter the G Number of the student you would like to modify");
-                    sm.modStudent(scnr.nextInt()); // if 2, a student will be modified
-                    scnr.nextLine();
+            case 2: //System.out.println("Enter the G Number of the student you would like to modify");
+                //sm.modStudent(scnr.nextInt()); // if 2, a student will be modified
+                //scnr.nextLine();
+                sm.modStudent();
                 break;
-            case 3: displayMain();
+            case 3:
+                displayMain();
                 break;
-            default: menuChoice = 0;
+            default:
+                System.out.println("Must select an option"); // default case
+                studentMenu();
         }
-
-        //System.out.println(menuChoice);
+        displayMain(); // displays the main menu after
     }
 
-    /**
+    /***************************************************************
      * courseMenu.  Displays course menu and takes user input
      * to either add a course or go back to the main menu.
-     */
-    public static void courseMenu() {
+     **************************************************************/
+    public void courseMenu() {
         // initializing variables
-        CourseMenu cm = new CourseMenu(); // creating object of the course menu class
-        Scanner scnr = new Scanner(System.in); // scanner
         int userNum = 0;
-        int menuChoice = 0;
 
         // Displaying course menu
         System.out.println("Course Menu\n");
@@ -140,27 +113,27 @@ public class MainMenu {
         userNum = scnr.nextInt(); // taking user input for menu selection
 
         // action is based on user input
-        switch(userNum) {
-            case 1: cm.addCourse(); // if 1, a course will be added
+        switch (userNum) {
+            case 1:
+                cm.addCourse(); // if 1, a course will be added
                 break;
-            case 2: displayMain();
+            case 2:
+                displayMain(); // if 2, go back to main menu
                 break;
-            default: menuChoice = 0;
+            default:
+                System.out.println("Must choose an option"); // default case
+                courseMenu();
         }
-
-        //System.out.println(menuChoice);
+        displayMain();
     }
 
-    /**
+    /***************************************************************
      * recordsMenu.  Displays student menu and takes user input
      * to either add/modify a student or go back to the main menu.
-     */
-    public static void recordsMenu() {
+     **************************************************************/
+    public void recordsMenu() {
         // initializing variables
-        RecordsMenu rm = new RecordsMenu(); // creating object of the student menu class
-        Scanner scnr = new Scanner(System.in); // scanner
         int userNum = 0;
-        int menuChoice = 0;
 
         // Displaying records menu
         System.out.println("Records Menu\n");
@@ -174,20 +147,26 @@ public class MainMenu {
         userNum = scnr.nextInt(); // taking user input for menu selection
 
         // action is based on user input
-        switch(userNum) {
-            case 1: rm.addRecord(); // if 1, a record will be added
+        switch (userNum) {
+            case 1:
+                rm.addRecord(); // if 1, a record will be added
                 break;
-            case 2: rm.viewTranscript(); // if 2, a transcript will be displayed
+            case 2:
+                rm.viewTranscript(); // if 2, a transcript will be displayed
                 break;
-            case 3: rm.saveRecord(); // if 3, the record will be saved to disk
+            case 3:
+                rm.saveRecord(); // if 3, the record will be saved to disk
                 break;
-            case 4: rm.loadRecord(); // if 4, records will be loaded from disk
+            case 4:
+                rm.loadRecord(); // if 4, records will be loaded from disk
                 break;
-            case 5: displayMain(); // if 5, main menu will be displayed
+            case 5:
+                displayMain(); // if 5, main menu will be displayed
                 break;
-            default: menuChoice = 0;
+            default:
+                System.out.println("Must choose an option");
+                recordsMenu();
         }
-
-        //System.out.println(menuChoice);
+        displayMain();
     }
 }
