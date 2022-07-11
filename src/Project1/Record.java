389e416e841e6998bc907dfe63a313907556c394
sarah-wight <107153217+sarah-wight@users.noreply.github.com>
2022-07-11 16:08:21 -0400

    Almost Complete - harden code

--- src/Project1/Record.java
+++ src/Project1/Record.java
@@ -16,10 +16,15 @@ public class Record {
     private Course course;
     private float grade;
 
+    /*********************************************************
+     * Default Constructor.  Creates empty Record object
+     ********************************************************/
     public Record() {
     }
 
-    // need to harden the code (setters)
+    /*********************************************************
+     * Default Constructor.  Creates default Record object
+     ********************************************************/
     public Record(Date date, Student student, Course course, float grade) {
         this.date = date;
         this.student = student;
@@ -27,38 +32,71 @@ public class Record {
         this.grade = grade;
     }
 
+    /*********************************************************
+     * getDate.  returns the Date
+     ********************************************************/
     public Date getDate() {
         return date;
     }
 
+    /*********************************************************
+     * setDate.  sets the Date
+     * @Date date
+     ********************************************************/
     public void setDate(Date date) {
         this.date = date;
     }
 
+    /*********************************************************
+     * getStudent.  returns a Student
+     ********************************************************/
     public Student getStudent() {
         return student;
     }
 
+    /*********************************************************
+     * setStudent.  sets the student information
+     * @Student student
+     ********************************************************/
     public void setStudent(Student student) {
         this.student = student;
     }
 
+    /*********************************************************
+     * getCourse.  returns the Course
+     ********************************************************/
     public Course getCourse() {
         return course;
     }
 
+    /*********************************************************
+     * setCourse.  sets the Course
+     * @Course course
+     ********************************************************/
     public void setCourse(Course course) {
         this.course = course;
     }
 
+    /*********************************************************
+     * getGrade.  returns a float with the student's grade
+     ********************************************************/
     public float getGrade() {
         return grade;
     }
 
+    /*********************************************************
+     * setGrade.  sets the student's grade
+     * @float grade
+     ********************************************************/
     public void setGrade(float grade) {
-        this.grade = grade;
+        if(grade <= 4.0 && grade >= 0.0) {
+            this.grade = grade;
+        } else {
+            throw new IllegalArgumentException("Grade must be between 0.0 and 4.0");
+        }
     }
 
+    
     public String toString() {
         return //"Record for " + student.getFirstName() + "\n" +
                 "Date: " + date + "\n" +
