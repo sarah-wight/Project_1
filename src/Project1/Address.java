28d8b41bf2727dd1b66ec5e8d0b8dfaece74a424
sarah-wight <107153217+sarah-wight@users.noreply.github.com>
2022-07-10 20:41:52 -0400

    Update Address.java

--- src/Project1/Address.java
+++ src/Project1/Address.java
@@ -1,14 +1,13 @@
 package Project1;
-import java.util.ArrayList;
 
-/**
+/***********************************************
  * Address class.
  *
  * This file contains and edits the addresses.
  * @author Sarah Wight, Zack Peters, Mike Rosin
  * @date 7/6/2022
  *
- */
+ **********************************************/
 public class Address {
     // local variables
     private int number;
@@ -18,10 +17,10 @@ public class Address {
     private String state;
     private int zip;
 
-    /**
+    /***********************************************
      * Constructor.  Sets base state of an Address object to the
      * values passed in.
-     */
+     **********************************************/
     public Address(){
         this.number = 1;
         this.street = "Campus Dr.";
@@ -31,126 +30,141 @@ public class Address {
         this.zip = 49401;
     }
 
-    /**
+    /***********************************************
      * getNumber.  Returns the number of the students address.
      * @return int
-     */
+     **********************************************/
     public int getNumber() {
         return number;
     }
 
-    /**
+    /***********************************************
      * setNumber.  Sets the number of the student's address to the value passed in.
      * @int number
-     */
+     **********************************************/
     public void setNumber(int number) {
+
+        // need to fix if user enters nothing
         // number must be greater than 0
-        if(number > 0) {
+        if(number > 0 && number < 1000000) {
             this.number = number;
-        }
-        else {
-            throw new RuntimeException("Number must be greater than 0");
+        } else {
+            throw new RuntimeException("Number must be greater than 0 and less than 1000000");
         }
     }
 
-    /**
+    /***********************************************
      * getStreet.  Returns the street name of the students address.
      * @return String
-     */
+     **********************************************/
     public String getStreet() {
         return street;
     }
 
-    /**
+    /***********************************************
      * setStreet.  Sets the street of the student's address to the value passed in.
      * @String street
-     */
+     **********************************************/
     public void setStreet(String street) {
+
+        // checks length and that entry is not null
         if((street.length() < 50) && (street != "")) {
             this.street = street;
-        }
-        else {
-            throw new RuntimeException("Street must be less than 50 characters");
+        } else {
+            throw new RuntimeException("Street must be between 1 and 50 characters");
         }
     }
 
-    /**
+    /***********************************************
      * getLineTwo.  Returns line two of the students address.
      * @return String
-     */
+     **********************************************/
     public String getLineTwo() {
         return lineTwo;
     }
 
-    /**
+    /***********************************************
      * setLineTwo.  Sets line two of the student's address to the value passed in.
      * @String lineTwo
-     */
+     **********************************************/
     public void setLineTwo(String lineTwo) {
-        // can this be blank?
-        this.lineTwo = lineTwo;
+
+        // checks length
+        if((street.length() < 50)) {
+            this.lineTwo = lineTwo;
+        } else {
+            throw new RuntimeException("Street must be between 1 and 50 characters");
+        }
     }
 
-    /**
+    /***********************************************
      * getCity.  Returns what city the student's address is located in.
      * @return String
-     */
+     **********************************************/
     public String getCity() {
         return city;
     }
 
-    /**
+    /***********************************************
      * setCity.  Sets the city of the student's address to the value passed in.
      * @String city
-     */
+     **********************************************/
     public void setCity(String city) {
+
         // should we capitalize each word of the string
-        if(city != "") {
+        // checks length and that the entry is not null
+        if(city.length() < 50 && city != "") {
             this.city = city;
         }
         else {
-            throw new RuntimeException("Please enter a city name");
+            throw new RuntimeException("City name must be between 1 and 50 characters");
         }
     }
 
-    /**
+    /***********************************************
      * getState.  Returns what state the student's address is located in.
      * @return String
-     */
+     **********************************************/
     public String getState() {
         return state;
     }
 
-    /**
+    /***********************************************
      * setState.  Sets the state of the student's address to the value passed in.
      * @String state
-     */
+     **********************************************/
     public void setState(String state) {
-        // must be 2 letters only -> capitalize them?
-        // have to be letter characters not any other type of char
-        // could we do this using the ascii number ?
+
+        // checks length and null entry
         if((state != "") && (state.length() == 2)) {
             this.state = state;
+        } else {
+            throw new RuntimeException("Please enter a 2 letter state abbreviation");
         }
-        else {
-            throw new RuntimeException("Please enter a state abbreviation");
+
+        // regex found from java api - a through z or A through Z, inclusive (range)
+        if(state.matches("[a-zA-Z]+")) {
+            this.state = state;
+        } else {
+            throw new RuntimeException("State name can only contain letters");
         }
     }
 
-    /**
+    /***********************************************
      * getZip.  Returns the zipcode of the student's address.
      * @return int
-     */
+     **********************************************/
     public int getZip() {
         return zip;
     }
 
-    /**
+    /***********************************************
      * setZip.  Sets the zipcode of the student's address to the value passed in.
      * @int zip
-     */
+     **********************************************/
     public void setZip(int zip) {
-        // must be 1-99999 -> should add zeroes to make it 5 digits
+
+        // checks size of entry
         if(zip >= 1 && zip <= 99999) {
             this.zip = zip;
         }
@@ -159,10 +173,10 @@ public class Address {
         }
     }
 
-    /**
+    /***********************************************
      * toString.  Returns a formatted String with the student's address.
      * @return String
-     */
+     **********************************************/
     public String toString() {
         return number + " " + street + "\n" + city + ", " + state + " " + zip;
     }
