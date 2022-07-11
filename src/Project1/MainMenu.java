f982a326a899bbe7676a19ea65479a1fdddbc210
sarah-wight <107153217+sarah-wight@users.noreply.github.com>
2022-07-11 15:57:11 -0400

    Almost Complete - harden

--- src/Project1/MainMenu.java
+++ src/Project1/MainMenu.java
@@ -55,7 +55,6 @@ public class MainMenu {
                 displayMain();
                 break;
         }
-        displayMain();
     }
 
     /***************************************************************
