5d94b0058c4d6843415eeef5ce1a2b0b15a66778
sarah-wight <107153217+sarah-wight@users.noreply.github.com>
2022-07-10 20:44:08 -0400

    Update Course.java

--- src/Project1/Course.java
+++ src/Project1/Course.java
@@ -1,64 +1,73 @@
 package Project1;
-import java.util.ArrayList;
-import java.lang.Exception;
 
-/**
+
+/***********************************************
  * Course class.
  *
  * This file contains and edits the courses.
  * @author Sarah Wight, Zack Peters, Mike Rosin
  * @date 7/6/2022
  *
- */
+ **********************************************/
 public class Course {
-    //
+    // local variables
     private String prefix;
     private int number;
     private String title;
 
-    /**
+    /***********************************************
      * Constructor.  Sets base state of a Course object to the
      * values passed in.
-     */
+     **********************************************/
     public Course(){
         this.prefix = "ABC";
         this.number = 123;
         this.title = "Default Course";
     }
 
-    /**
+    /***********************************************
      * getPrefix.  Returns a String of the course prefix.
      * @return String
-     */
+     **********************************************/
     public String getPrefix() {
         return prefix;
     }
 
-    /**
+    /***********************************************
      * setPrefix.  Sets the course prefix.
      * @String prefix.
-     */
+     **********************************************/
     public void setPrefix(String prefix) {
+
+        // checks length of entry
         if (prefix.length() == 3){
             this.prefix = prefix;
-        }
-        else
-            throw new RuntimeException("Enter a course Prefix that is only 3 characters");
-    }
+        } else
+            throw new RuntimeException("Enter a course prefix that is 3 characters");
 
-    /**
+        // regex found from java api - a through z or A through Z, inclusive (range)
+        if(prefix.matches("[a-zA-Z]+")) {
+            this.prefix = prefix;
+        } else {
+            throw new RuntimeException("course prefix can only contain letters");
+        }
+    }
+    
+    /***********************************************
      * getNumber.  Returns an int of the course number.
      * @return int
-     */
+     **********************************************/
     public int getNumber() {
         return number;
     }
 
-    /**
+    /***********************************************
      * setNumber.  Sets the course number.
      * @String number.
-     */
+     **********************************************/
     public void setNumber(int number) {
+
+        // checks size of integer
         if(999 >= number && number > 0) {
             this.number = number;
         }
@@ -67,19 +76,21 @@ public class Course {
         }
     }
 
-    /**
+    /***********************************************
      * getTitle.  Returns a String of the course title.
      * @return String
-     */
+     **********************************************/
     public String getTitle() {
         return title;
     }
 
-    /**
+    /***********************************************
      * setTitle.  Sets the class title.
      * @String title.
-     */
+     **********************************************/
     public void setTitle(String title) {
+
+        // checks for null entry
         if(title != "") {
             this.title = title;
         }
@@ -88,10 +99,10 @@ public class Course {
         }
     }
 
-    /**
+    /***********************************************
      * toString.  Returns a formatted String with the course prefix, number, and title.
      * @return String
-     */
+     **********************************************/
     public String toString() {
         return prefix + " " + number + ": " + title;
     }
