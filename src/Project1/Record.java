6bfc32626c816554d2abd4278c1694f0c1c5bc74
sarah-wight <107153217+sarah-wight@users.noreply.github.com>
2022-07-11 12:27:19 -0400

    Need to harden setters

--- src/Project1/Record.java
+++ src/Project1/Record.java
@@ -1,49 +1,30 @@
 package Project1;
-import java.util.ArrayList;
 
-/**
+import java.util.*;
+
+/*******************************************************
  * Record class.
  *
  * This file contains and edits the student records.
  * @author Sarah Wight, Zack Peters, Mike Rosin
- * @date 7/6/2022
+ * @date 7/11/2022
  *
- */
+ ******************************************************/
 public class Record {
-    private Date date; // need a date class
+    private Date date;
     private Student student;
     private Course course;
     private float grade;
-    private SMS sms = new SMS();
-
-    /*public static void main(String[] args) {
-        Date date = new Date();
-        Student sw =  new Student();
-        Course course = new Course();
-        Record record1 = new Record(date, sw, course, 4.0f);
-        System.out.println(record1);
-
-    }*/
 
     public Record() {
-
     }
 
-    public Record(Student student) {
-        this.student = student;
-    }
-
-    public Record(Course course) {
-        this.course = course;
-    }
-
+    // need to harden the code (setters)
     public Record(Date date, Student student, Course course, float grade) {
         this.date = date;
         this.student = student;
         this.course = course;
         this.grade = grade;
-
-        sms.addRecord(this);
     }
 
     public Date getDate() {
@@ -67,8 +48,7 @@ public class Record {
     }
 
     public void setCourse(Course course) {
-        Record record = new Record(course);
-        //this.course = course;
+        this.course = course;
     }
 
     public float getGrade() {
